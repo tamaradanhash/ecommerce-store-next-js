13b4d1afac801be716d565cb0b5c1c9fe63e4650
Tamara Danhash <tamaradanhash1989@gmail.com>
2020-07-22 19:15:07 +0200

    Change name to Navigator+Use react-bootstrap/Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,80 +1,39 @@
-import Link from 'next/link';
-export default function Navbar() {
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import * as Unicons from '@iconscout/react-unicons';
+export default function Navigator() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>
-              {' '}
-              Home
-              <img src="/homepage.svg" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>
-              {' '}
-              About Us
-              <img src="/about.svg" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            <a>
-              {' '}
-              Contact Us
-              <img src="/contact.svg" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/products">
-            <a>
-              {' '}
-              Products
-              <img src="/bed.svg" />
-            </a>
-          </Link>
-        </li>
-      </ul>
-      <style jsx>
-        {`
-          ul {
-            margin: auto;
-            margin-top: 30px;
-            position: relative;
-            width: 900px;
-            height: 70px;
-            background: #ffffff;
-            display: flex;
-          }
-          ul li {
-            list-style: none;
-            width: 200px;
-            height: 75px;
-            left: 221px;
-            text-align: center;
-            margin-top: 20px;
-          }
-          ul li a {
-            font-family: Tenor Sans;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 24px;
-            line-height: 28px;
-            text-align: center;
-            color: #606568;
-          }
-          ul li a img {
-            position: absolute;
-            width: 30px;
-            height: 30px;
-          }
-        `}
-      </style>
-    </div>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      style={{ background: '#a28b51' /*'#ffac41'*/ /*'#856131'*/ }}
+    >
+      <Navbar.Brand href="/">
+        Home
+        <Unicons.UilHome />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/contact">Contact us</Nav.Link>
+          <NavDropdown title="Products" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Living room</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Electronics</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Kids</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Offers</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="/myCart">
+            Cart
+            <Unicons.UilShoppingCart />
+          </Nav.Link>
+          <Nav.Link eventKey={2} href="/login">
+            login
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
